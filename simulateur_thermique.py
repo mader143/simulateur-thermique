793d47e869c29c27e6af79b35dcc1aef02fac3fa
Maya Déry <maya.dery.1@ulaval.ca>
2026-01-19 20:33:11 -0500

    test code trouvé en ligne

--- simulateur_thermique.py
+++ simulateur_thermique.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as mpl
+import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
 
 # longueur =
 # largeur = 
@@ -8,20 +10,18 @@ import matplotlib.pyplot as mpl
 # position_act =
 # dimensions_act = 
 # diffusivity = 
-# dx =
-# dy = dx
 # dt = 0.5*(dx)**2/diffusivity
 
-def plaque(longueur, largeur, T_init, T_act, position_act, dimensions_act, grid_res=0.1e-3):
+def plaque(longueur, largeur, T_init, T_act, position_act, dimensions_act, dx=0.1e-3):
     '''
         Fonction de géométrie pour une plaque 2D qui retourne un array avec les conditions limites
         Arguments: longueur et largeur de la plaque, température initiale (de la pièce et donc de la plaque), 
         température, dimensions et position de l'actuateur, résolution
         Retourne un array de la température dans la plaque
     '''
+    dy = dx
 
-    nx = int(longueur / grid_res)
-    ny = int(largeur / grid_res) 
+    nx, ny = int(longueur / dx), int(largeur / dy)
 
     plaque = np.full((ny, nx), T_init)
 
