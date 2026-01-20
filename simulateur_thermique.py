e951d7a444cba51ffa744e3b090dbdfc727b0b36
Maya Déry <maya.dery.1@ulaval.ca>
2026-01-19 19:21:45 -0500

    debut fonction plaque

--- simulateur_thermique.py
+++ simulateur_thermique.py
@@ -1,4 +1,12 @@
-#allo
-# test commit marianne
-#yolo mia
-#testa
+import numpy as np
+import matplotlib.pyplot as mpl
+
+
+# fonction de géométrie pour une plaque 2D qui retourne un array avec les conditions limites
+
+def plaque(longueur, largeur, T_init, grid_res=0.1e-3):
+
+    nx = int(longueur / grid_res)
+    ny = int(largeur / grid_res) 
+
+    plaque = np.full((ny, nx), T_init)
