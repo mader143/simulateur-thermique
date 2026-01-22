497d83a949f5add22b26b437e20d25a0c1384677
Maya Déry <maya.dery.1@ulaval.ca>
2026-01-22 17:46:18 -0500

    start

--- simulateur_thermique.py
+++ simulateur_thermique.py
@@ -1,28 +1,64 @@
 import numpy as np
 import matplotlib.pyplot as mpl
-import matplotlib.animation as animation
-from matplotlib.animation import FuncAnimation
 
-# longueur (m) = 117.28e-3
-# largeur = 61.57e-3
-# T_init =
-# T_act =
-# position_act =
+
+longueur = 117.28e-3 #en m
+largeur = 61.57e-3 #en m
+T_init = 21 + 273 #T pièce en kelvins
+T_act = 30 + 273
+t_simulation = 100 #en secondes
+# loc_act =
 # dimensions_act = 
-# diffusivity = 
-# dt = 0.5*(dx)**2/diffusivity
+k = 205
+rho = 2700
+cp = 900
+alpha = k / (rho * cp)
+h_conv = 20
 
-def plaque(longueur, largeur, T_init, T_act, position_act, dimensions_act, dx=0.1e-3):
-    '''
-        Fonction de géométrie pour une plaque 2D qui retourne un array avec les conditions limites
-        Arguments: longueur et largeur de la plaque, température initiale (de la pièce et donc de la plaque), 
-        température, dimensions et position de l'actuateur, résolution
-        Retourne un array de la température dans la plaque
-    '''
-    dy = dx
+dx=0.1e-3
+dy = dx
+dt = 0.5*(dx)**2/alpha #à ajuster
 
-    nx, ny = int(longueur / dx), int(largeur / dy)
+nx, ny, nt = int(longueur / dx), int(largeur / dy), int(t_simulation/dt)
 
-    plaque = np.full((ny, nx), T_init)
+plaque = np.full((ny, nx, nt), T_init)
 
-    return plaque
+thermistance1 = np.zeros(nt)
+therm1_loc = (14.87e-3, ((61.57e-3)/2)) #j'ai mis position en y a mi chemin
+thermistance2 = np.zeros(nt)
+therm2_loc = (59.35e-3, ((61.57e-3)/2))
+thermistance3 = np.zeros(nt)
+therm3_loc = (104.99e-3, ((61.57e-3)/2))
+
+# for t in range(nt):
+#     for i in range(nx):
+#         for j in range(ny):
+
+#             if i == 0:
+#             plaque[i, j, t] += ajout normal
+#             plaque[i, j, t] += ajout de convection de côté x
+
+#             elif i == nx-1:
+#             plaque[i, j, t] += ajout normal
+#             plaque[i, j, t] += ajout de convection de côté x
+
+#             elif j == 0:
+#             plaque[i, j, t] += ajout normal
+#             plaque[i, j, t] += ajout de convection de côté y
+
+#             elif j == ny-1:
+#             plaque[i, j, t] += ajout normal
+#             plaque[i, j, t] += ajout de convection de côté y
+
+#             else:
+#             elif i == nx-1:
+#             plaque[i, j, t] += ajout normal
+
+#             plaque[i, j, t] += ajout de convection de haut et bas de la plaque??
+
+# ajout des températures des thermistances pour faire un graphique plus tard
+
+#      thermistance1[t] = plaque[therm1_loc, t]
+#      thermistance2[t] = plaque[therm2_loc, t]
+#      thermistance3[t] = plaque[therm3_loc, t]
+            
