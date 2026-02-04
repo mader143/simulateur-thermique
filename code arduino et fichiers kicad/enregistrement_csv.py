7009f903d2fa5813c0d8a1ee7172c4054479d58c
sabrinadem <SADEM50@ulaval.ca>
2026-02-04 18:26:31 -0500

    identification thermistances!

--- code arduino et fichiers kicad/enregistrement_csv.py
+++ code arduino et fichiers kicad/enregistrement_csv.py
@@ -9,11 +9,11 @@ import pandas as pd
 # Pour exécuter le script: pip install pyserial pandas matplotlib
 
 # ============ CONFIGURATION ============
-PORT = 'COM5'  # Changez au besoin (COM3, COM4, etc.)
+PORT = 'COM6'  # Changez au besoin (COM3, COM4, etc.)
 BAUDRATE = 9600
 
 # EMPLACEMENT D'ENREGISTREMENT
-CHEMIN_ENREGISTREMENT = r'C:\Users\mathi\OneDrive\Bureau\simulateur-thermique\code arduino et fichiers kicad' 
+CHEMIN_ENREGISTREMENT = r'C:\Users\sabri\OneDrive\Desktop\uni\design\simulateur-thermique\code arduino et fichiers kicad\ident_therm' 
 
 # Nom du fichier (avec horodatage automatique)
 NOM_FICHIER = f'temperature_{datetime.now().strftime("%Y%m%d_%H%M%S")}.csv'
