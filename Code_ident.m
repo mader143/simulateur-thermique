fc577cc276a56f0953c30b1349f929ce717917bc
Maya Déry <maya.dery.1@ulaval.ca>
2026-02-10 06:51:01 -0500

    njkw

--- Code_ident.m
+++ Code_ident.m
@@ -3,7 +3,11 @@ data = load("file name");
 t = data(1);
 T = data(2);
 
-sys = tfest();
+np = 2;   % poles
+nz = 1;   % zeros
+iodelay = NaN % retard inconnu
+
+sys = tfest(data, np, nz, iodelay);
 
 N = sys.Numerator;
 D = sys.Denominator;
