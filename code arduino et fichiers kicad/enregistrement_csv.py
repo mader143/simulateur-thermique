81861582dd224e023f5b2a1a2563c5c777b75901
alex turcotte <altur112@ulaval.ca>
2026-02-16 17:51:38 -0500

    h trouvé yééééé

--- code arduino et fichiers kicad/enregistrement_csv.py
+++ code arduino et fichiers kicad/enregistrement_csv.py
@@ -9,11 +9,11 @@ import pandas as pd
 # Pour exécuter le script: pip install pyserial pandas matplotlib
 
 # ============ CONFIGURATION ============
-PORT = 'COM6'  # Changez au besoin (COM3, COM4, etc.)
+PORT = 'COM5'  # Changez au besoin (COM3, COM4, etc.)
 BAUDRATE = 9600
 
 # EMPLACEMENT D'ENREGISTREMENT
-CHEMIN_ENREGISTREMENT = r'C:\Users\sabri\OneDrive\Desktop\uni\design\simulateur-thermique\code arduino et fichiers kicad\ident_therm' 
+CHEMIN_ENREGISTREMENT = r'C:\Users\alext\OneDrive\Bureau' 
 
 
 # Nom du fichier (avec horodatage automatique)
