9c46fb426f2b36b91f04e6a72ecc4ad7c0a0f6c9
alex turcotte <altur112@ulaval.ca>
2026-02-25 10:43:48 -0500

    fdjhj

--- code arduino et fichiers kicad/ident_h/curve_fit_h.py
+++ code arduino et fichiers kicad/ident_h/curve_fit_h.py
@@ -1,25 +1,33 @@
 import numpy as np
 import scipy.optimize as sp
 import matplotlib.pyplot as plt
+import os
 
-file = open("/Users/mayadery/Desktop/H26/design 2/simulateur-thermique-1/code arduino et fichiers kicad/ident_h/temperature_20260216_172323.csv", 'r')
-line = file.readline()
+
+dossier_script = os.path.dirname(os.path.abspath(__file__))
+fichier_csv = os.path.join(dossier_script, "temperature_20260216_172323.csv")
 
 temps = []
 Temp = []
 
+
 i = 0
-for line in file:
-    i += 1
-    if i > 1:
-        data = line.split(",")
-        temps.append(float(data[0]))
-        Temp.append(float(data[1]))
+
+with open(fichier_csv, 'r') as file:
+    next(file)
+    #next(file)  # saute la première ligne (header)
+
+    for line in file:
+        i += 1
+        if i > 1:
+            data = line.split(",")
+            temps.append(float(data[0]))
+            Temp.append(float(data[1]))
 
 t = np.array(temps)
 T = np.array(Temp)
 print(T)
-t -= t[0]
+t2 = t - 10.530
 
 def exponentielle(t, tau, a, c):
     return a*np.exp(-t/tau) + c
@@ -35,7 +43,8 @@ print(popt[0])
 
 plt.figure()
 plt.plot(t, T, '.b', label='Données expérimentales')
-plt.plot(t, exponentielle(t, popt[0], popt[1], popt[2]), 'r-', label='Fit exp')
+plt.plot(t, exponentielle(t, popt[0], popt[1], popt[2]), 'r-', label='Courbe d\'ajustement')
 plt.xlabel('Temps [s]')
 plt.ylabel('Température [°C]')
+plt.legend()
 plt.show()
