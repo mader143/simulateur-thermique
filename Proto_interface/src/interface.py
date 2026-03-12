b30f67f5e4a73d769254a48b4b49dd49d72ac009
alex turcotte <altur112@ulaval.ca>
2026-03-12 13:24:04 -0400

    ajustement_boutons_proto

--- Proto_interface/src/interface.py
+++ Proto_interface/src/interface.py
@@ -23,39 +23,44 @@ class InterfaceProto:
         )
         upload_btn.pack(side=tk.LEFT, padx=10)
         
-        tk.Label(self.root, text="Température à ambiante (°C):", font=("Arial", 12)).pack(pady=5)
-        self.temperature_entry_amb = tk.Entry(self.root, width=10, font=("Arial", 12))
+
+        frame_temp = tk.Frame(self.root)
+        frame_temp.pack(pady=5)
+
+        tk.Label(frame_temp, text="Température ambiante (°C):", font=("Arial", 12)).grid(row=0, column=0, padx=10, pady=5, sticky="w")
+
+        self.temperature_entry_amb = tk.Entry(frame_temp, width=10, font=("Arial", 12))
         self.temperature_entry_amb.insert(0, self.temperature_ambiante)
-        self.temperature_entry_amb.pack(pady=5)
-       
-        
-        tk.Label(self.root, text="Température à atteindre (°C):", font=("Arial", 12)).pack(pady=5)
-        self.temperature_entry_att = tk.Entry(self.root, width=10, font=("Arial", 12))
-        self.temperature_entry_att.insert(0, self.temperature_voulue)
-        self.temperature_entry_att.pack(pady=5)
+        self.temperature_entry_amb.grid(row=0, column=1, padx=5, pady=5)
 
-
-        contrast_frame = tk.Frame(self.root)
-        contrast_frame.pack(pady=10)
-
-       
-
-        self.contrast_value_label = tk.Label(
-            contrast_frame, text=f"Value: N/A",
-            font=("Arial", 12), bg="lightyellow", relief="solid", borderwidth=1, padx=10
+        apply_btn_amb = tk.Button(
+            frame_temp, text="Appliquer", command=self.valider_temp_ambiante,
+            font=("Arial", 12), width=8
         )
-        self.contrast_value_label.pack(side=tk.LEFT, padx=5)
-        
-        self.count_label = tk.Label(self.root, bg="white", relief="solid", borderwidth=2, fg="darkblue")
-  
+        apply_btn_amb.grid(row=0, column=2, padx=5)
+
+
+        tk.Label(frame_temp, text="Température à atteindre (°C):", font=("Arial", 12)).grid(row=1, column=0, padx=10, pady=5, sticky="w")
+
+        self.temperature_entry_att = tk.Entry(frame_temp, width=10, font=("Arial", 12))
+        self.temperature_entry_att.insert(0, self.temperature_voulue)
+        self.temperature_entry_att.grid(row=1, column=1, padx=5, pady=5)
+
+        apply_btn_att = tk.Button(
+            frame_temp, text="Appliquer", command=self.valider_temp_voulue,
+            font=("Arial", 12), width=8
+        )
+        apply_btn_att.grid(row=1, column=2, padx=5)
+    
+
+    
 
     def valider_temp_ambiante(self):
         try:
-            value = float(self.temperature_ambiante.get())
+            value = float(self.temperature_entry_amb.get())
             if -30 <= value <= 60:
                 self.temperature_ambiante = value
-                self.contrast_value_label.config(text=f"Value: {self.temperature_ambiante:.2f}")
-                if self.temperature_voulue is not None:
+                if self.temperature_voulue is isinstance(value, (float)):
                     self.demarrer_proto()
             
             else:
@@ -65,16 +70,18 @@ class InterfaceProto:
 
     def valider_temp_voulue(self):
         try:
-            value = float(self.temperature_voulue.get())
-            self.temperature_voulue = value
-            if self.temperature_ambiante is not None:
-                self.demarrer_proto()
+            value = float(self.temperature_entry_att.get())
+            if 10 <= value <= 45:
+                self.temperature_voulue = value
+                if self.temperature_ambiante is isinstance(value, (int, float)):
+                    self.demarrer_proto()
             else:
                 raise ValueError
         except ValueError:
             messagebox.showerror("Erreur", "Veuillez entrer un nombre valide pour la température à atteindre.")
 
     def demarrer_proto(self):
+
         print(f"Température Ambiante: {self.temperature_ambiante} °C")
         print(f"Température à atteindre: {self.temperature_voulue} °C") 
 
