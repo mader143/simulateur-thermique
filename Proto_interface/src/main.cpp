421eef39e30c18602a3686a7f47ac92e02570741
Maya Déry <maya.dery.1@ulaval.ca>
2026-03-18 15:00:39 -0400

    arreterproto2

--- Proto_interface/src/main.cpp
+++ Proto_interface/src/main.cpp
@@ -146,13 +146,13 @@ void loop() {
 
         if (v1 != -1 && v2 != -1 && v3 != -1 && v4 != -1) {
             setpoint   = valeurs.substring(0, v1).toFloat();
-            T0_celsius = valeurs.substring(v1 + 1, v2).toFloat();
-            a0         = valeurs.substring(v2 + 1, v3).toFloat();
-            // a1         = valeurs.substring(v3 + 1, v4).toFloat();
+            a0         = valeurs.substring(v1 + 1, v2).toFloat();
+            //a1         = valeurs.substring(v2 + 1, v3).toFloat();
             a1 = -a0
-            a2         = valeurs.substring(v4 + 1).toFloat();
+            a2         = valeurs.substring(v3 + 1, v4).toFloat();
+            //T0_celsius = valeurs.substring(v4 + 1).toFloat();
 
-            T0 = T0_celsius + 273.15;
+            //T0 = T0_celsius + 273.15;
             
             Serial.println("ACK:CONFIG_COMPLETE"); 
         }
